9c53befc715014f9a6acadfce2a1f6d892dbd6fe
ifuriouscoder <indianpushpendra1@gmail.com>
2024-02-06 18:54:05 +0530

    06 FEBRUARY 2024 #Namaste dunia basic learning

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Namaste Duniya ! main wapas aa rha hoon coding ke is suhane se safar mein!!"<<endl;
+    cout<<"Enter n: ";
+    cin>>n;
+    cout<<n;
+}
